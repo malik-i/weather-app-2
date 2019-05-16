1dfb886fb9923ac55b53910f144b77d018479b1b
malik iscandari <malikiscan@gmail.com>
2019-05-15 21:52:48 -0700

    placed the data in a table, just need to apply styling

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,36 @@
 import React, {Component} from 'react'
+import {Table} from 'react-bootstrap'
+import'./App.css'
 
 class Weather extends Component {
     render(){
         return(
-            <ul>
-                <li>City: {this.props.city}</li>
-                <li>Country: {this.props.country}</li>
-                <li>Temperature: {this.props.temperature}</li>
-                <li>Humidity: {this.props.humidity}</li>
-                <li>Description: {this.props.description}</li>
-            </ul>
+            <div className="weather-data" >
+                <Table className="weather-table" striped bordered hover>
+                    <tbody>
+                        <tr>
+                            <td><strong>City</strong></td>
+                            <td>{this.props.city}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Country</strong></td>
+                            <td>{this.props.country}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Temperature</strong></td>
+                            <td>{this.props.temperature}°F</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Humidity</strong></td>
+                            <td>{this.props.humidity}%</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Description</strong></td>
+                            <td>{this.props.description}</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
         )
     }
 }
